56c6d9daf7d0160a6c67d3913934791805ba8b06
Lalit Mehra <lokeshmehra7039@gmail.com>
2026-02-19 15:48:10 +0530

    add 217 leetcode problem contains duplicate

---
+++ Array/Easy/ContainsDuplicate.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool containsDulplicate(int *arr, int n){
+    sort(arr,arr+n);
+    for(int i = 0 ; i< n; i++){
+        cout<<arr[i]<<",";
+    }
+    cout<<endl;
+    for(int i = 1 ; i < n ; i++){
+        if(arr[i] == arr[i-1]){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    int arr[] = {1,1,1,3,3,4,3,2,4,2};
+    int n = sizeof(arr)/sizeof(int);
+    cout<<containsDulplicate(arr,n);
+    return 0;
+}
