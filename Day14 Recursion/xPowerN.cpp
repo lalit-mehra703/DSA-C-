12faaa407ad5805647eb87809abac8f4eb3b8f2a
Lalit Mehra <lokeshmehra7039@gmail.com>
2026-03-14 14:32:25 +0530

    x power n

---
+++ Day14 Recursion/xPowerN.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int pow(int x , int n){
+    if(n == 0){
+        return 1;
+    }
+    int halfPow = pow(x , n/2);
+    int halfPowSquare = halfPow * halfPow;
+
+    if( n % 2 != 0){
+        return x * halfPowSquare;
+    }
+    return halfPowSquare;
+}
+
+int main(){
+    cout<<pow(2, 10);
+    return 0;
+}
