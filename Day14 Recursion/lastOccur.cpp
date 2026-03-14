76cfb1c324272409c83a92e55d6feb862a71f5a9
Lalit Mehra <lokeshmehra7039@gmail.com>
2026-03-14 10:06:20 +0530

    last occurrence

---
+++ Day14 Recursion/lastOccur.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// int lastOccur(vector<int> arr , int i, int target){
+
+//     if(i < 0){
+//         return -1;
+//     }
+    
+//     if(arr[i] == target){
+//         return i;
+//     }
+//     return lastOccur(arr, i-1 , target);
+// } 
+
+
+int lastOccur(vector<int> arr , int i , int target){
+    if(i == arr.size()) return -1;
+    int idxFound = lastOccur(arr, i+1 , target);
+    if(idxFound == -1 && arr[i] == target){
+        return i;
+    }
+    return idxFound;
+}
+
+
+int main(){
+    vector<int> arr = {1,2,3,3,3,4};
+    // int i = arr.size()-1;
+    // cout<<lastOccur(arr, i , 3);
+    cout<<lastOccur(arr, 0 , 3);
+    return 0;
+}
