5e4b4960f6c1020bf6561b5e58e754bffd22f843
Lalit Mehra <lokeshmehra7039@gmail.com>
2026-04-05 16:15:16 +0530

    solve nQueene problem

---
+++ Day19 Backtracking/NQueens.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include <vector>
+using namespace std; 
+
+void printBoard( vector<vector<char>>board){
+    int n = board.size();
+    for(int i = 0 ; i < n ; i ++){
+        for(int j = 0 ; j < n ; j++){
+            cout<<board[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"----------"<<endl;
+}
+
+bool isSafe(vector<vector<char>> board, int row , int col){
+    int n = board.size();
+    //check vertical safety
+    for(int i = 0 ; i < row ; i++){
+        if(board[i][col] == 'Q'){
+            return false;
+        }
+    }
+
+    // check left digonal safety 
+    for( int i = row, j = col ; i >= 0 && j >= 0 ; i--,j--){
+        if(board[i][j] == 'Q'){
+            return false;
+        }
+    }
+
+
+    //check right digonal safety 
+
+    for(int i = row , j = col; i >= 0 && j < n ; i--,j++){
+        if(board[i][j] == 'Q'){
+            return false;
+        }
+    }
+    return true;
+}
+
+void nQuens(vector<vector<char>> board, int row){
+    int n = board.size();
+    if(row == n){
+        printBoard(board);
+        return;
+    }
+
+    for(int i = 0 ; i < n ; i++){
+        if(isSafe(board, row, i)){
+            board[row][i] = 'Q';
+            nQuens(board, row+1);
+            board[row][i] = '.';
+        }
+        
+    }
+}
+
+int main(){
+    vector<vector<char>>board;
+    int n = 10;
+    for(int i = 0 ; i < n ; i++){
+        vector<char>newRow;
+        for(int j = 0 ; j < n ; j++){
+            newRow.push_back('.');
+        }
+        board.push_back(newRow);
+    }
+
+    nQuens(board, 0);
+    return 0;
+}
